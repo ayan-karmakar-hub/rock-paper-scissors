ba6536ae1ef60506c9863cf4cf39e141b9335200
Ayan Karmakar <ayan_karmakar@utexas.edu>
2020-04-30 19:31:10 -0500

    commented code

--- play-game.js
+++ play-game.js
@@ -1,5 +1,5 @@
-
-//alert("Welcome to Rock-Paper-Scissors.");
+// setting up my variables that keep track of
+// the round number and each player's score
 var pScore = 0;
 var cScore = 0;
 let playerScore = document.querySelector('#player-score');
@@ -9,14 +9,34 @@ let compText = compScore.innerText;
 let roundNumber = document.querySelector('#round-number');
 let roundText = roundNumber.innerText;
 
+// Create an event listener for each of the images
+// corresponding to rock,paper,and scissors
 let choices = document.querySelectorAll('.choice');
 choices.forEach((choice)=> choice.addEventListener('click',startRound));
+
+// simulates the computer's choice by picking
+// a random number between 0 and 2 inclusive
+function computerPlay(){
+    let value = Math.floor(Math.random()*3);
+    let result = (value == 0)? "rock":
+    (value==1)? "paper": "scissors";
+    return result;
+}
+
+// set up playRound with the correct parameters
+// I am unsure how to pass in functions with parameters
+// to event listeners, so this is my workaround.
 function startRound(){
     playRound(this.id, computerPlay());
 }
 
+// the main part of the program. This will increment
+// the score of the player with the winning choice
+// If a player reaches 5 points, the game will end
 function playRound(playerSelection,computerSelection){
 
+    // determines the player who won the round (if any)
+    // and increments the scores appropriately
     switch(true){
         case playerSelection === "rock" && computerSelection == "scissors":
         case playerSelection === "paper" && computerSelection == "rock":
@@ -34,6 +54,7 @@ function playRound(playerSelection,computerSelection){
             cScore++;     
     }
 
+    // updates the round number and score display
     playerText = playerText.substr(0,8) + pScore;
     playerScore.innerText = playerText;
     compText = compText.substr(0,10) + cScore;
@@ -41,33 +62,40 @@ function playRound(playerSelection,computerSelection){
     roundText = roundText.substr(0,15) + (cScore+pScore+1);
     roundNumber.innerText = roundText;
 
+    // checks for game end conditions
     if(pScore >= 5 || cScore >= 5){
         endGame();
     }
 }
 
+// handles the end of the game. The game will 
+// restart if the player types yes when prompted.
 function endGame(){
+    let message = "";
     if(pScore > cScore){
-        console.log("You won the game!");
+        message+= "You won the game!\n";
     } else if (cScore > pScore){
-        console.log("The computer won the game!");
+        message += "The computer won the game!\n"
     } else {
-        console.log("It was a tie!");
+        message+= "It was a tie!\n";
     }
-    console.log(`Final Scores--> Player:${pScore} | Computer:${cScore}`);
-    console.log("Thanks for playing.");
+    message+=`Final Scores--> Player: ${pScore} | Computer: ${cScore}\n`;
+    message+="Thanks for playing.";
+    alert(message);
     
     pScore = 0;
     cScore = 0;
 
     let response = prompt('Enter "yes" to play again');
-    if(response.toLowerCase() === "yes"){
+    if(response && response.toLowerCase() === "yes"){
         reset();
     } else {
         choices.forEach((choice)=> choice.removeEventListener('click',startRound));
     }
 }
 
+// helper function that resets the 
+// round number and scores to 0
 function reset(){
     playerText = playerText.substr(0,8) + 0;
     playerScore.innerText = playerText;
@@ -75,11 +103,4 @@ function reset(){
     compScore.innerText = compText;
     roundText = roundText.substr(0,15) + 1;
     roundNumber.innerText = roundText;
-}
-
-function computerPlay(){
-    let value = Math.floor(Math.random()*3);
-    let result = (value == 0)? "rock":
-    (value==1)? "paper": "scissors";
-    return result;
 }
